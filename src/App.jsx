21ff6bd42a6c08f4986079a16de5ba1d147ec31e
Juan Britos <juanbtitos@gmail.com>
2025-10-18 20:57:03 -0300

    Added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Nav } from "./components/Nav/Nav";
+import { ItemListContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Nav />
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/detail/:id" element={undefined} />
+        </Routes>
       </BrowserRouter>
     </>
   );
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ItemDetail } from "../ItemDetail/ItemDetail";
+
+export const ItemDetailContainer = () => {
+  const [detail, setDetail] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    fetch("/data/data.json").then((res) => {
+      if (!res.ok) {
+        throw new Error("Hubo un porblema al buscar productos");
+      }
+      return res.json();
+    }).then((data) => {
+      const found = data.find(prod => prod.id === id);
+      if (found) {
+        setDetail(found);
+      } else {
+        throw new Error("Producto no encontrado");
+      }
+    }).catch((err) => {
+      console.error(err);
+    });
+  }, [id]);
+  return (
+    <main>
+      {Object.keys(detail).length ? (<ItemDetail detail={detail} />) : (<p>Cargando ...</p>)}
+    </main>
+  );
+};
