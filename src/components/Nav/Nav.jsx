2e9f0e9c0de96e6ce4596469a21b286c23d3229b
Juan Britos <jbritos@dc.uba.ar>
2025-10-18 16:29:56 -0300

    Added component folder structure and Nav component

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+export const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/category/dulce">Dulce</Link>
+        </li>
+        <li>
+          <Link to="/category/saldo">Salado</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
