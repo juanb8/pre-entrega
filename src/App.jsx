e209a074a221534b178487ec2b48b5b8f722fdda
Juan Britos <jbritos@dc.uba.ar>
2025-10-25 23:01:51 -0300

    Modified the linking

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Nav } from "./components/Nav/Nav";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { CartProvider } from "./context/CartContext/CartProvider";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -15,6 +16,11 @@ function App() {
             <Route path="/" element={<ItemListContainer />} />
             <Route path="/detail/:id" element={<ItemDetailContainer />} />
           </Routes>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
         </CartProvider>
       </BrowserRouter>
     </>
