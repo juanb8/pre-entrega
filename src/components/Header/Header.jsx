ee7d79f1124d37e0282b4f466c6fa77ae8a13691
Juan Britos <jbritos@dc.uba.ar>
2025-12-05 19:50:51 -0300

    modified the header again

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import { Nav } from "../Nav/Nav";
 import "./Header.css";
+import logo from "public/controller-7692999_1280";
 export const Header = () => {
   return (
     <header className="header">
       <Link to={"/"} className="log-container">
-        <img className="logo" src="public/controller-7692999_1280.png" />
+        <img className="logo" src={logo} />
       </Link>
       <Nav />
     </header>
