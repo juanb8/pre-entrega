d933d48f9361d5b5cb84d1d21f983da2dedb801c
Juan Britos <jbritos@dc.uba.ar>
2025-12-09 14:53:45 -0300

    trying the logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import { Nav } from "../Nav/Nav";
 import "./Header.css";
-// import logo from "public/controller-7692999_1280";
+import logo from "../../../public/controller-7692999_1280";
 export const Header = () => {
   return (
     <header className="header">
       <Link to={"/"} className="log-container">
-        <h2 className="logo">LOGO </h2>
+        <img src={logo} className="logo" />
+        // <h2 className="logo">LOGO </h2>
       </Link>
       <Nav />
     </header>
